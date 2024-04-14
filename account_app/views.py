bdb87285fa6ecf2a89b6abf4e72d4de082055836
H-M-Nizum <hmnizum1714032@gmail.com>
2024-04-14 11:31:10 +0600

    Artwork model api create

--- account_app/views.py
+++ account_app/views.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
-from .serializers import ArtistRegistrationSerializer, ArtistLoginSerializer, ArtistProfileSerializer
+from .models import ArtistModel, ArtworkModel
+from .serializers import ArtistRegistrationSerializer, ArtistLoginSerializer, ArtistProfileSerializer, ArtworkSerializer
 from .renderers import UserRenderer
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
+from django.views.decorators.csrf import csrf_exempt
 
 # Generate Token Manually
 def get_tokens_for_user(user):
@@ -61,13 +64,7 @@ class ArtistProfileView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-from rest_framework import generics, permissions
-from .models import ArtworkModel
-from .serializers import ArtworkSerializer
-from rest_framework import permissions
-from rest_framework.views import APIView
-from django.views.decorators.csrf import csrf_exempt
-
+# using coustom permission method for art owner
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -78,17 +75,31 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed to the owner of the artwork.
         return obj.artist == request.user
 
-class ArtworkListCreateAPIView(generics.ListCreateAPIView):
-    queryset = ArtworkModel.objects.all()
-    serializer_class = ArtworkSerializer
+# create artwork and disply artwork list
+class ArtworkListCreateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self, serializer):
-        serializer.save(artist=self.request.user)
+    def get(self, request, format=None):
+        artworks = ArtworkModel.objects.all()
+        serializer = ArtworkSerializer(artworks, many=True)
+        return Response({"message": "successfully get Artwork All data", "data" : serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = ArtworkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(artist=request.user)
+            return Response({"message": "successfully create a new instance", "data" : serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Update and delete artwork
 class ArtworkUpdateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get(self, request, pk, format=None):
+        artwork = self.get_object(pk)
+        serializer = ArtworkSerializer(artwork)
+        return Response({"message": "successfully get Artwork single instance", "data" : serializer.data}, status=status.HTTP_200_OK)
 
     @csrf_exempt
     def put(self, request, pk, format=None):
@@ -96,7 +107,7 @@ class ArtworkUpdateAPIView(APIView):
         serializer = ArtworkSerializer(artwork, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response({"message": "successfully Update single instance", "data" : serializer.data}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @csrf_exempt
@@ -105,13 +116,13 @@ class ArtworkUpdateAPIView(APIView):
         serializer = ArtworkSerializer(artwork, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response({"message": "successfully Partial update single instance", "data" : serializer.data}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         artwork = self.get_object(pk)
         artwork.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "successfully delete single instance"}, status=status.HTTP_204_NO_CONTENT)
 
     def get_object(self, pk):
         try:
